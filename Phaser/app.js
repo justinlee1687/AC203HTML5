e64429d6b82fc31cf291a3619e842257d25ef381
justinlee1687 <justinlee1687@gmail.com>
2017-03-11 11:15:24 +0800

    update

--- Phaser/app.js
+++ Phaser/app.js
@@ -25,9 +25,9 @@ function create() {
 	ground.body.immovable= true;
 
 	//add the 2 ledges
-	var ledge = platforms.create(-150,250,'ground')
+	var ledge = platforms.create(-150,250,'ground');
 	ledge.body.immovable= true;
-	ledge = platforms.create(400,400,'ground')
+	ledge = platforms.create(400,400,'ground');
 	ledge.body.immovable= true;
 
 }
