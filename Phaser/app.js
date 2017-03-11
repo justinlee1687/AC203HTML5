cace26597df57167dd85d798c65400c3873ce141
justinlee1687 <justinlee1687@gmail.com>
2017-03-11 12:10:07 +0800

    update

--- Phaser/app.js
+++ Phaser/app.js
@@ -41,9 +41,22 @@ function create() {
 	player.body.gravity.y = 300;
 	player.body.collideWorldBounds = true;
 
+	//add cursors as keyboard input to control the player
+	cursors = game.input.keyboard.createCursorKeys();
+
 }
 
 function update() {
 	//collision between player and platforms
 	game.physics.arcade.collide(player,platforms);
+
+	//when left or right key pressed
+	if(cursors.left.isDown){
+		player.body.velocity.x= -150;
+	}
+	if(cursors.right.isDown){
+		player.body.velocity.x= 150;
+	}
+
+
 }
