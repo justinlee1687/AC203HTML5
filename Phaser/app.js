c191aec0d371412ae9dcf480880cf8d77421c3ba
justinlee1687 <justinlee1687@gmail.com>
2017-03-11 12:13:06 +0800

    update

--- Phaser/app.js
+++ Phaser/app.js
@@ -53,11 +53,11 @@ function update() {
 	//when left or right key pressed
 	if(cursors.left.isDown){
 		player.body.velocity.x= -150;
-		play.ananimations.play('left');
+		play.animations.play('left');
 	}
 	if(cursors.right.isDown){
 		player.body.velocity.x= 150;
-		play.ananimations.play('right');
+		play.animations.play('right');
 	}
 
 
