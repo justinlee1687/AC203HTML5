f0d76d216504fd90494fa4350c7194127db33699
justinlee1687 <justinlee1687@gmail.com>
2017-03-18 12:05:30 +0800

    update

--- Phaser/app.js
+++ Phaser/app.js
@@ -88,9 +88,11 @@ function update() {
 		star.kill()
 		score += 10;
 		scoreText.text="Score: "+score;
+		for ( var i=0; i<2; i++){
 		var star = stars.create(Math.random()*750, 20, 'star');
 		star.body.gravity.y=200;
 		star.body.bounce.y=0.6+Math.random()*0.3;
+	}
 	}
 
 
